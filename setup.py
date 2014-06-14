01e16634f8357f475725454ddb15c8fa84c58975
Tiago Antao <tiagoantao@gmail.com>
2014-06-14 19:22:36 +0100

    housekeeping

--- setup.py
+++ setup.py
@@ -9,13 +9,43 @@ manual:
 
 '''
 from distutils.command.install import install
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
+import os
+
+import sphinx
 
 
 __version__ = None  # PEP 8
 for line in open('genomics/__init__.py'):
     if (line.startswith('__version__ = ')):
         exec(line.strip())
+
+
+class Sphinx(Command):
+    user_options = []
+    description = 'sphinx'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # metadata contains information supplied in setup()
+        metadata = self.distribution.metadata
+        # package_dir may be None, in that case use the current directory.
+        src_dir = (self.distribution.package_dir or {'': ''})['']
+        src_dir = os.path.join(os.getcwd(),  src_dir)
+        # Run sphinx by calling the main method, '--full' also adds a conf.py
+        sphinx.apidoc.main(
+            ['', '--full', '-H', metadata.name, '-A', metadata.author,
+             '-V', metadata.version, '-R', metadata.version,
+             '-o', os.path.join('doc', 'source'), src_dir])
+        # build the doc sources
+        sphinx.main(['', os.path.join('doc', 'source'),
+                     os.path.join('doc', 'build')])
+
 
 setup(
     name='pygenomics',
@@ -37,4 +67,3 @@ setup(
 )
 
 #TODO dependencies
-#sphinx-pypi-upload
