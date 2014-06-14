3c0924d1af8408b0e6ccf5c0775026b39ef93db2
Tiago Antao <tiagoantao@gmail.com>
2014-06-14 20:53:55 +0100

    sphinx

--- setup.py
+++ setup.py
@@ -9,12 +9,39 @@ manual:
 
 '''
 from distutils.command.install import install
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
+import os
 
 __version__ = None  # PEP 8
 for line in open('genomics/__init__.py'):
     if (line.startswith('__version__ = ')):
         exec(line.strip())
+
+
+class Sphinx(Command):
+    user_options = []
+    description = 'build sphinx documentation'
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # metadata contains information supplied in setup()
+        metadata = self.distribution.metadata
+        src_dir = os.path.join(os.getcwd(),  'genomics')
+        # Run sphinx by calling the main method, '--full' also adds a conf.py
+        from sphinx import apidoc
+        apidoc.main(
+            ['', '-f', '-H', 'Python Genomics', '-A', metadata.author,
+             '-V', metadata.version, '-R', metadata.version,
+             '-o', os.path.join('doc', 'api'), src_dir])
+        # build the doc sources
+        os.chdir('doc')
+        os.system('make html')
+        os.chdir('..')
 
 
 setup(
@@ -33,6 +60,7 @@ setup(
     ],
     cmdclass={
         'install': install,
+        'doc': Sphinx
     },
 )
 
