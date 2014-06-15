52700da8c649a5c0457b6a42fd47720573daae39
Tiago Antao <tiagoantao@gmail.com>
2014-06-15 11:17:14 +0100

    database

---
+++ genomics/db/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.db
+   :synopsis: Database for distributed write-once mapreduce ops
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
