2ac86fad42db552c04ae8dac815a08e72c9ae5fb
Tiago Antao <tiagoantao@gmail.com>
2014-06-15 11:17:51 +0100

    MapReduce

---
+++ genomics/mapreduce/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.popgen
+   :synopsis: Population genetics/genomics
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
