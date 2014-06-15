d3454bf1c36fb731743454e3bf8d19d18db8e752
Tiago Antao <tiagoantao@gmail.com>
2014-06-15 18:20:30 +0100

    database - ongoing

--- genomics/db/__init__.py
+++ genomics/db/__init__.py
@@ -9,46 +9,44 @@
 .. moduleauthor:: Tiago Antao <tra@popgen.net>
 
 '''
+import abc
 import os
 
 
 class DBException(Exception):
-    pass  # TODO sort this out
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
-class Node:
+class Node(metaclass=abc.ABCMeta):
     '''A Database node
 
-    :param base_dir: Base directory
-    :param node_file: File that will hold the node
-    :param start_pos: Start position in the current key contect
-    :param size: Size of the node
+    :param db: Database
     :param to_write: Write node?
-    :param is_sparse: Sparse implementation
+    :param desired_pos: Any position inside the node
 
     A node is a file that holds a (small) portion of the data. Its size is
     defined by the database granularity. It should be the size of the map
     operation on the map reduce framework. It should very easily fit in
     memory.
     '''
-    def __init__(self, base_dir, node_file, start_pos, size,
-                 to_write, is_sparse):
-        self.base_dir = base_dir
-        self.node_file = node_file
-        self.start_pos = start_pos
-        self.size = size
+    def __init__(self, db, to_write, desired_pos=None):
+        self.db = db
         self.to_write = to_write
-        self.is_sparse = is_sparse
-        self._vals = [None] * size
+        if to_write:
+            self._vals = [None] * db.granularity
 
     def assign(self, position, value):
         if not self.to_write:
-            raise
+            raise DBException('Need to be in write mode to assign')
         self.vals[position - self.start_pos] = value
 
     def commit(self):
         if not self.to_write:
-            raise
+            raise DBException('Need to be in write mode to commit')
         node_dir = os.sep.join([self.base_dir] +
                                self.node_file.split(os.sep)[:-1])
         os.makedirs(node_dir)
@@ -61,7 +59,7 @@ class DB:
     '''A Database
 
     :param base_dir: Base directory
-    :param key_class: Key c;ass
+    :param node_class: Node class
     :param granularity: Record granularity
     :param is_sparse: Sparse implementation
 
@@ -69,42 +67,45 @@ class DB:
     database will hold at most those. A non-sparse database will hold
     precisely those (save for the very last node).
 
-    The key can be mostly anything that ends in something that is an integer.
-    For example is can be a (chromosome, position)
+    The node can be mostly anything structured that ends in something that is
+    an integer.  For example is can be a (chromosome, position)
     '''
-    def __init__(self, base_dir, key_fun, granularity, is_sparse=False):
+    def __init__(self, base_dir, key_class, granularity, is_sparse=False):
         self.base_dir = base_dir
-        self.key_fun = key_fun
+        self.key_class = key_class
         self.granularity = granularity
         self.is_sparse = is_sparse
 
     def get_write_node(self, **kwargs):
-        node_file = self.key_fun(self.granularity, kwargs)
-        return Node(self.base_dir, node_file, True, self.is_sparse)
+        node = self.key_class(self.granularity, kwargs)
+        return node.get_node(kwargs)
 
     def find_missing_nodes(self):
         pass
 
 
 class Key:
-    def __init__(self, granularity):
+    def __init__(self, db, granularity):
+        self.db = db
         self.granularity = granularity
 
     def enumerate_nodes(self):
         pass
 
-    def get_node(self, **kwargs):
+    def get_node(self, to_write, **kwargs):
         pass
 
 
 class GenomeKey(Key):
-    def __init__(self, granularity, genome):
-        Key.__init__(self, granularity)
+    def __init__(self, db, granularity, genome):
+        Key.__init__(self, db, granularity)
         self.genome = genome
 
     def enumerate_nodes(self):
         pass
 
-    def get_node(self, chromosome, position):
-        return os.sep.join('%s' % str(chromosome),
-                           '%9d' % position // self.granularity)
+    def get_node(self, to_write, chromosome, position):
+        fname = os.sep.join('%s' % str(chromosome),
+                            '%9d' % position // self.granularity)
+        return Node(self.db.base_dir, fname, self.granularity, to_write,
+                    self.db.is_sparse)
