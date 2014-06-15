185faf1eda392bd309bc9af39f7580bdc6fcfb58
Tiago Antao <tiagoantao@gmail.com>
2014-06-15 11:17:09 +0100

    blaze

---
+++ genomics/blaze/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.blaze
+   :synopsis: Blaze interface
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
