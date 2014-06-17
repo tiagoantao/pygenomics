a19a26f6a203b07beddc6bb4b15dfbecb0e4cff6
Tiago Antao <tra@popgen.net>
2014-06-17 10:19:04 +0100

    configuration

--- genomics/config.py
+++ genomics/config.py
@@ -9,3 +9,31 @@
 .. moduleauthor:: Tiago Antao <tra@popgen.net>
 
 '''
+import configparser
+import os
+
+config_file = os.path.expanduser('~/.config/pygenomics/main.conf')
+# This can be configured before loading of the main module to read another file
+
+
+class Config:
+    '''Configuration object
+
+    :param config_file: The config file to use
+
+    The default config file is defined above and can be changed before doing
+    import genomics
+    '''
+    def __init__(self, config_file=config_file):
+        self.config_file = config_file
+
+    def load_config(self):
+        config = configparser.ConfigParser()
+        config.read(self.config_file)
+        self.grid = config['main']['grid']
+        if self.grid == 'Local':
+            self.grid_limit = config['grid.local']['limit']
+            if self.grid_limit.find('.') > -1:
+                self.grid_limit = float(self.grid_limit)
+            else:
+                self.grid_limit = int(self.grid_limit)
