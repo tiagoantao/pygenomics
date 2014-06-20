d35cb71840dbe3e2c357219a9b06215f9e374c8d
Tiago Antao <tiagoantao@gmail.com>
2014-06-20 18:36:46 +0100

    genepop module

---
+++ genomics/popgen/genepop/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.popgen.genepop
+   :synopsis: Genepop processing
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
