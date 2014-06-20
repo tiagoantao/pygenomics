46b6642873ff3beeb73972f682d7bc53d113104e
Tiago Antao <tiagoantao@gmail.com>
2014-06-20 18:37:06 +0100

    smartpaca

---
+++ genomics/popgen/pca/smart.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.popgen.pca.smart
+   :synopsis: Eigen soft smart PCA
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
