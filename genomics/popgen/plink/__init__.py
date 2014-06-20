17515d1034546f3f0d1d2254a1db9ae10cc89a47
Tiago Antao <tiagoantao@gmail.com>
2014-06-20 18:38:47 +0100

    plink module

---
+++ genomics/popgen/plink/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.popgen.plink
+   :synopsis: PLINK processing
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
