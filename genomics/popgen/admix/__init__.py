100f998353f0f24e763b0901211268714da04d5e
Tiago Antao <tiagoantao@gmail.com>
2014-07-30 13:42:53 +0100

    admixture plotting

---
+++ genomics/popgen/admix/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+.. module:: genomics.popgen.admix
+   :synopsis: Admixture
+   :noindex:
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+
+'''
