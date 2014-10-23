b9304c52ad67be955d8f270b685282bc2e2027e9
Tiago Antao <tra@popgen.net>
2014-10-23 16:21:57 +0100

    NeEstimator2 processing

---
+++ genomics/popgen/ne2/__init__.py
@@ -0,0 +1,284 @@
+# -*- coding: utf-8 -*-
+'''
+.. module: genomics.popgen.ne2
+   :synopsis: This module provides code to work with NeEstimator2.
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+"""
+
+import pprint
+
+
+class Record:
+    """Holds information from a NeEstimator2 OUTPUT file.
+
+    @ivar freqs_used  Frequencies used as a cutoff. The list
+                       might include None (no cutoff)
+
+    @ivar  ld          List of LD data
+                       Composed of a list (size = cutoffs) dictionary with
+                       HMean
+                       IndepComp
+                       OvRSquare
+                       ExpRSquareSample
+                       EstNe
+                       ParaNe
+                       IGNORING JACKNIFE for now
+    @ivar  het         List of Het data
+                       Composed of a list (size = cutoffs) dictionary with
+                       HMean
+                       IndepAlleles
+                       WeiMeanD
+                       EstNeb
+                       IGNORING PARAMETRIC AND JACKNIFE For now
+    @ivar  coanc       List of Coanc data
+                       Composed of a SINGLE dictionary with
+                       HMean
+                       OvF1
+                       EstNeb
+                       IGNORING PARAMETRIC AND JACKNIFE For now
+    @ivar  temporal    List of temporal info.
+                       Composed of dict with
+                       sampleId1
+                       sampleId2
+                       generation1
+                       generation2
+                       results
+                       results it itself another dictionary with the
+                       method as key (Pollak,Nei/Tajima,Jorde/Ryman) and
+                       a content of a dict per cutoff of
+                           HMean
+                           IndepAlleles
+                           Fk
+                           Fline
+                           Ne
+
+    """
+    def __init__(self):
+        self.freqs_used = []
+        self.ld = []
+        self.het = []
+        self.coanc = []
+        self.temporal = []
+
+    def __str__(self):
+        myRep = ""
+        pp = pprint.PrettyPrinter()
+        myRep += "LD\n"
+        for elem in self.ld:
+            myRep += pp.pformat(elem)
+            myRep += "\n"
+        myRep += "HetExcess\n"
+        for elem in self.het:
+            myRep += pp.pformat(elem)
+            myRep += "\n"
+        myRep += "Coanc\n"
+        for elem in self.coanc:
+            myRep += pp.pformat(elem)
+            myRep += "\n"
+        for elem in self.temporal:
+            myRep += "Samples: %d %d\n" % (elem["sampleId1"],
+                     elem["sampleId2"])
+            myRep += "Generations: %d %d\n" % (elem["generation1"],
+                     elem["generation2"])
+            for stat, result in elem["results"].items():
+                myRep += stat
+                myRep += " "
+                myRep += pp.pformat(result)
+                myRep += "\n"
+            myRep += "\n"
+        return myRep
+
+
+def _getVals(guard, line, fun=float):
+    """Gets a line of values from Ne2 output and converts it.
+
+    @param guard String that splits text from values (typically =)
+    @param line Line to parse
+    @param fun Function to convert string, typically but int also common
+    """
+    vals = []
+    line = line[line.find(guard) + len(guard):]
+    toks = filter(lambda x: x != "", line.split(" "))
+    if fun == float:
+        fun = lambda x: None if x == "0+" or x == "Infinite" else float(x)
+    vals = [fun(x) for x in toks]
+    return vals
+
+
+def _readTemp(results, f):
+    method = f.readline().strip().rstrip()[1:-1]
+    cutCases = []
+    results[method] = cutCases
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for val in vals:
+        cutCases.append({"HMean": val})
+    #l = f.readline().rstrip()
+    #vals = _getVals("=", l, int)
+    #for i in range(len(vals)):
+    #    cutCases[i]["IndepAlleles"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        cutCases[i]["Fk"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        cutCases[i]["Fline"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        cutCases[i]["Ne"] = vals[i]
+
+    l = f.readline().rstrip()
+    l = f.readline().rstrip()
+    l = f.readline().rstrip()
+    vals = _getVals("c", l)
+    for i in range(len(vals)):
+        cutCases[i]["ParaTemp"] = [vals[i]]
+    l = f.readline().rstrip()  # 0.975
+    vals = _getVals(" ", l)
+    for i in range(len(vals)):
+        cutCases[i]["ParaTemp"].append(vals[i])
+    l = f.readline().rstrip()
+    l = f.readline().rstrip()
+
+
+def _doTemporal(rec, f):
+    """Parse Temporal part."""
+    f.readline()  # First empty line
+    l = f.readline()  # freqs
+    if len(rec.freqs_used) == 0:
+        rec.freqs_used = _getVals("Used", l.rstrip())
+    l = f.readline()
+    while l != "":
+        l = l.rstrip()
+        if l.find("=======") > -1:
+            l = f.readline().rstrip()
+            if l.startswith("Samples"):
+                tempCalc = {}
+                rec.temporal.append(tempCalc)
+                l = l[7:].strip()
+                toks = l.split("&")
+                tempCalc["sampleId1"] = int(toks[0][:toks[0].find("[")])
+                tempCalc["sampleId2"] = int(toks[1][:toks[1].find("[")])
+                l = f.readline().rstrip()[11:].strip()
+                toks = l.split("&")
+                tempCalc["generation1"] = float(toks[0].rstrip())
+                tempCalc["generation2"] = float(toks[1].rstrip())
+                l = f.readline()
+                l = f.readline()
+                # Independent alleles TBD
+                tempCalc["results"] = {}
+                l = f.readline()
+                while l.find("-----") > -1:
+                    _readTemp(tempCalc["results"], f)
+                    l = f.readline()
+            else:
+                return
+        else:
+            l = f.readline()
+
+
+def _doLD(rec, f):
+    "Parse LD."
+    myLDs = []
+    l = f.readline().rstrip()
+    while l.find("Harmonic") == -1:
+        l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for val in vals:
+        myLDs.append({"HMean": val})
+    l = f.readline().rstrip()
+    vals = _getVals("=", l, int)
+    for i in range(len(vals)):
+        myLDs[i]["IndepComp"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        myLDs[i]["OvRSquare"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        myLDs[i]["ExpRSquareSample"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        myLDs[i]["EstNe"] = vals[i]
+    l = f.readline().rstrip()  # Empty
+    l = f.readline().rstrip()  # 95% CIs for Ne^
+    l = f.readline().rstrip()  # 0.025
+    vals = _getVals("c", l)
+    for i in range(len(vals)):
+        myLDs[i]["ParaNe"] = [vals[i]]
+    l = f.readline().rstrip()  # 0.975
+    vals = _getVals(" ", l)
+    for i in range(len(vals)):
+        myLDs[i]["ParaNe"].append(vals[i])
+    rec.ld.append(myLDs)
+
+
+def _doExcess(rec, f):
+    """Parse Heterozygosity Excess."""
+    myHets = []
+    l = f.readline().rstrip()
+    while l.find("Harmonic") == -1:
+        l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for val in vals:
+        myHets.append({"HMean": val})
+    l = f.readline().rstrip()
+    vals = _getVals("=", l, int)
+    for i in range(len(vals)):
+        myHets[i]["IndepAlleles"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        myHets[i]["WeiMeanD"] = vals[i]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    for i in range(len(vals)):
+        myHets[i]["EstNeb"] = vals[i]
+    rec.het.append(myHets)
+
+
+def _doCoanc(rec, f):
+    """Parse coancestry."""
+    myCoanc = {}
+    l = f.readline().rstrip()
+    while l.find("Harmonic") == -1:
+        l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    myCoanc["HMean"] = vals[0]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    myCoanc["OvF1"] = vals[0]
+    l = f.readline().rstrip()
+    vals = _getVals("=", l)
+    myCoanc["EstNeb"] = vals[0]
+    rec.coanc.append(myCoanc)
+
+
+def parse(f):
+    """Parses a NeEstimator2 output file.
+
+       @param f File descriptor
+    """
+    rec = Record()
+    l = f.readline()
+    while l != "":
+        if len(rec.freqs_used) == 0 and l.find("Lowest Allele") > -1:
+            rec.freqs_used = _getVals("Used", l.rstrip())
+        if l.find("LINKAGE DISEQUILIBRIUM METHOD") > -1:
+            _doLD(rec, f)
+        if l.find("HETEROZYGOTE EXCESS METHOD") > -1:
+            _doExcess(rec, f)
+        if l.find("MOLECULAR COANCESTRY METHOD") > -1:
+            _doCoanc(rec, f)
+        if l.find("TEMPORAL METHOD") > -1:
+            _doTemporal(rec, f)
+        l = f.readline()
+    return rec
---
+++ genomics/popgen/ne2/controller.py
@@ -0,0 +1,117 @@
+'''
+.. module: controller
+   :synopsis: This module allows to control NeEstimator2
+   :copyright: Copyright 2014 by Tiago Antao
+   :license: GNU Affero, see LICENSE for details
+
+.. moduleauthor:: Tiago Antao <tra@popgen.net>
+'''
+
+__docformat__ = "javadoc en"  # This has to change
+
+
+import os
+import sys
+
+
+class NeEstimator2Controller:
+    def __init__(self, ne2_dir=None):
+        """Initializes the controller.
+
+        The initializer checks for existance and executability of binaries.
+
+        @param ne2_dir is the directory where NeEstimator2 is. If empty
+                       the system will try to find the binary on the PATH.
+
+        """
+        self.platform = sys.platform
+        if self.platform == 'win32':
+            self.bin_name = 'Ne2.exe'
+        elif self.platform == "darwin":  # Mac missing
+            self.bin_name = 'Ne2M'
+        else:
+            self.bin_name = 'Ne2L'
+        if ne2_dir:
+            self.ne2_dir = ne2_dir
+        else:
+            for myDir in os.environ["PATH"].split(os.pathsep):
+                if self.bin_name in os.listdir(myDir) and os.access(
+                    myDir + os.sep + self.bin_name, os.X_OK
+                ) and os.path.isfile(myDir + os.sep + self.bin_name):
+                    self.ne2_dir = myDir
+                    break
+        dir_contents = os.listdir(self.ne2_dir)
+        if self.bin_name in dir_contents:
+            if not os.access(self.ne2_dir + os.sep +
+                             self.bin_name, os.X_OK):
+                raise IOError("NeEstimator2 not executable")
+        else:
+            raise IOError("NeEstimator2 not available")
+
+    def run_neestimator2(self, in_dir, gen_file, out_dir, out_file,
+                         crits=[], LD=True, hets=False,
+                         coanc=False, temp=None,
+                         monogamy=False, options={}):
+        """Executes NeEstimator2.
+
+
+        @param in_dir   The input directory
+        @param gen_file The genepop file
+        @param out_dir  The output directory
+        @param out_file Where the output will be stored
+        @param LD       Do LD method
+        @param hets     Do excessive heterozygosity
+        @param coanc    Do Molecular coancestry
+        @param temp     List of generations for the temporal method
+                        (None = Not do)
+
+        XXX document options
+
+        """
+        in_name = 'in.le' + str(os.getpid())
+        inf = open(in_name, 'w')
+        val = 0
+        if LD:
+            val += 1
+        if hets:
+            val += 2
+        if coanc:
+            val += 4
+        if temp is not None:
+            val += 8
+        inf.write("%d 0\n" % val)
+        inf.write("%s/\n" % in_dir)
+        inf.write("%s\n" % gen_file)
+        inf.write("2\n")  # Genepop
+        inf.write("%s/\n" % out_dir)
+        inf.write("%s\n" % out_file)
+        inf.write("%d\n" % len(crits))
+        if len(crits) == 0:
+            inf.write("\n")
+        else:
+            inf.write("%s -1\n" % " ".join([str(x) for x in crits]))
+        inf.write("%d\n" % (1 if monogamy else 0))  # Random mating
+        if temp:
+            # Plan II only for now (and single pop only)
+            inf.write("0 %s\n" % " ".join(map(lambda x: str(x), temp[0])))
+        inf.close()
+        if len(options) == 0:
+            opt_txt = ''
+        else:
+            opt_name = 'opt.le' + str(os.getpid())
+            opt_txt = ' o:' + opt_name
+            opf = open(opt_name, 'w')
+            opf.write('0 0\n')
+            opf.write('0\n')
+            opf.write('0\n')
+            opf.write('%s\n' % options.get('burrows', '0'))
+            opf.write('1\n')
+            opf.write('1\n')
+            opf.write('0\n')
+            opf.write('0\n')
+            opf.close()
+        os.system(self.ne2_dir + os.sep + self.bin_name +
+                  ' i:' + in_name + opt_txt + ' >/dev/null 2>&1')
+        os.remove(in_name)
+        if len(options) > 0:
+            os.remove(opt_name)
