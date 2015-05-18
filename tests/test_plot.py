c50911ffc389c0d8b8e75eb6ebfef61d0fc39008
Tiago Antao <tiagoantao@gmail.com>
2015-05-18 14:31:14 +0100

    suitable backend for travis-ci

--- tests/test_plot.py
+++ tests/test_plot.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from os import path
-import tempfile
-from genomics.plot import GridGenomePlot
-from genomics.organism import genome_db
 
 
 def test_basic_chart():
+    from os import path
+    import tempfile
+    import matplotlib
+    matplotlib.use('Agg')
+    from genomics.plot import GridGenomePlot
+    from genomics.organism import genome_db
     ggp = GridGenomePlot(genome_db['Ag'], 2)
     with tempfile.TemporaryDirectory() as tmp:
         fname = '%s/test.png' % tmp
